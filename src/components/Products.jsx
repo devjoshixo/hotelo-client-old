4b224f2b6f958073418e4f74811d6cbcc539ba75
devjoshixo <djoshi923@gmail.com>
2023-08-02 14:56:55 +0530

    Display

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import hotelPhoto from '../assets/marten-bjork-n_IKQDCyrG0-unsplash.jpg';
 import hotelPhoto from '../assets/display.jpg';
 import classes from './Products.module.css';
 
 const Products = () => {
+  const [loaded, setLoaded] = useState(false);
   return (
     <>
-      <div className={classes.displayImage}>
-        <img src={hotelPhoto} />
+      <div
+        className={`${classes.displayImage} ${loaded ? '' : classes.notloaded}`}
+      >
+        <img
+          src={hotelPhoto}
+          onLoad={() => {
+            setLoaded(true);
+          }}
+        />
         <h2 className={classes.offer}>Grab Upto 50% on selected Hotels</h2>
       </div>
     </>
