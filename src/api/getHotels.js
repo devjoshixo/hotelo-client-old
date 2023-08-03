5aa9834973090346d30ec9a3048d58e3352c1c7d
devjoshixo <djoshi923@gmail.com>
2023-08-04 01:00:33 +0530

    Api usage

--- src/api/getHotels.js
+++ src/api/getHotels.js
@@ -1,6 +1,6 @@
 const getHotels = async () => {
-  const url =
-    'https://hotels-com-provider.p.rapidapi.com/v2/hotels/search?checkin_date=2023-09-26&adults_number=1&region_id=2872&checkout_date=2023-09-27&locale=en_US&sort_order=REVIEW&domain=US&star_rating_ids=3%2C4%2C5&payment_type=PAY_LATER%2CFREE_CANCELLATION&lodging_type=HOTEL%2CHOSTEL%2CAPART_HOTEL&price_max=500&amenities=WIFI%2CPARKING&children_ages=4%2C0%2C15&page_number=1&price_min=10&guest_rating_min=8&meal_plan=FREE_BREAKFAST&available_filter=SHOW_AVAILABLE_ONLY';
+  const key = Math.floor(Math.random() * 50);
+  const url = `https://hotels-com-provider.p.rapidapi.com/v2/hotels/search?checkin_date=2023-09-26&adults_number=1&region_id=2872&checkout_date=2023-09-27&locale=en_US&sort_order=REVIEW&domain=US&star_rating_ids=3%2C4%2C5&payment_type=PAY_LATER%2CFREE_CANCELLATION&lodging_type=HOTEL%2CHOSTEL%2CAPART_HOTEL&price_max=1000&amenities=WIFI%2CPARKING&children_ages=4%2C0%2C15&page_number=1&price_min=10&guest_rating_min=8&meal_plan=FREE_BREAKFAST&available_filter=SHOW_AVAILABLE_ONLY`;
   const options = {
     method: 'GET',
     headers: {
